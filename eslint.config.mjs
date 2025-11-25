d5e159bffd6fb81c0f7d7e2f61029d5090ebc283
severusDude <fajarsaladin05@gmail.com>
2025-11-25 10:50:27 +0700

    style(eslint): update configuration to include rules for unused variables

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,19 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_", // Ignore unused function arguments
+          varsIgnorePattern: "^_", // Ignore unused variables
+          caughtErrorsIgnorePattern: "^_", // Ignore unused catch clause parameters
+          ignoreRestSiblings: true, //  Ignore unused properties when using object destructuring
+        },
+      ],
+    },
+  },
 ]);
 
 export default eslintConfig;
