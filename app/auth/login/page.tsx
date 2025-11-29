d5eb91ad56538f5c04f298f54301737696bacbd5
Lutfi Fajar <60343449+severusDude@users.noreply.github.com>
2025-11-29 18:03:25 +0700

    Add auth login and register form UI (#5)
    * feat(auth): add layout component for authentication pages
    
    * feat(auth): implement registration form with validation
    
    * feat(auth): add registration page with form component
    
    * feat(auth): implement login form component
    
    * fix(auth): add offset to register login link underline

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,5 @@
+import LoginForm from "@/app/components/forms/login-form";
+
+export default function Page() {
+  return <LoginForm className="w-full max-w-xs" />;
+}
---
+++ app/auth/register/page.tsx
@@ -0,0 +1,5 @@
+import RegisterForm from "@/components/forms/register-form";
+
+export default function Page() {
+  return <RegisterForm className="w-full max-w-xs" />;
+}
