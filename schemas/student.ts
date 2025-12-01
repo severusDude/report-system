39141da53cbfad2a94aa9be846631a7cb4619f73
severusDude <fajarsaladin05@gmail.com>
2025-12-01 17:45:02 +0700

    fix(students): set student upsert schema id to optional for create

--- schemas/student.ts
+++ schemas/student.ts
@@ -3,7 +3,9 @@ import z from "zod";
 import { Gender } from "@/generated/prisma/enums";
 import { Prisma } from "@/generated/prisma/client";
 
-export const StudentCreateInput = z.object({
+// Schema for upserting a student from the form
+export const StudentUpsertSchema = z.object({
+  id: z.string().optional(),
   name: z
     .string()
     .min(1, "Full name is required")
@@ -15,5 +17,23 @@ export const StudentCreateInput = z.object({
   citizenship: z.string().min(1, "Citizenship is required"),
   familyCardNumber: z.string().min(1, "Family Card Number is required"),
   birthPlace: z.string().min(1, "Birth Place is required"),
-  dateOfBirth: z.date().min(1, "Date of Birth is required"),
+  dateOfBirth: z.date(),
+});
+
+// Schema for creating a student
+export const StudentCreateInput = StudentUpsertSchema.omit({
+  id: true,
 }) satisfies z.ZodType<Prisma.StudentUncheckedCreateInput>;
+
+// Schema for updating a student (same as creating a student)
+export const StudentUpdateInput =
+  StudentCreateInput satisfies z.ZodType<Prisma.StudentUncheckedUpdateInput>;
+
+export const StudentUpsertInput = z.object({
+  where: z.object({
+    id: z.string(),
+  }) satisfies z.ZodType<Prisma.StudentWhereUniqueInput>,
+
+  create: StudentCreateInput,
+  update: StudentUpdateInput,
+});
