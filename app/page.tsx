088a0b29c6022a2af5abf6d83509c19387c108fd
severusDude <fajarsaladin05@gmail.com>
2025-12-05 16:40:18 +0700

    feat(students): add basic student page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,335 @@
+import Link from "next/link";
 import Image from "next/image";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Role } from "@/generated/prisma/enums";
+import { Edit, HomeIcon, MoonIcon, SearchIcon } from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarInset,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { Separator } from "@/components/ui/separator";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { NavUser } from "@/components/nav-user";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+
+const session = await auth.api.getSession({
+  headers: await headers(),
+});
+
+type Data = {
+  student: {
+    nisn: string;
+    name: string;
+    gender: string;
+    birthPlace: string;
+    dateOfBirth: string;
+    phoneNumber: string;
+    email: string;
+    address: string;
+    imageUrl: string;
+  };
+  enrollments: {
+    name: string;
+    grade: string;
+    teacher: {
+      name: string;
+      email: string;
+    };
+    attendances: {
+      session: number;
+      date: string;
+      status: "present" | "absent" | "sick" | "on_leave";
+    }[];
+  }[];
+};
+
+const data: Data = {
+  student: {
+    nisn: "1234567890",
+    name: "John Doe",
+    gender: "male",
+    birthPlace: "Jakarta",
+    dateOfBirth: "2000-01-01",
+    phoneNumber: "08123456789",
+    email: "student@example.com",
+    address: "Jl. Contoh, Jakarta",
+    imageUrl: "https://via.placeholder.com/150",
+  },
+  enrollments: [
+    {
+      name: "Math",
+      grade: "A",
+      teacher: {
+        name: "Mr. Smith",
+        email: "z3r0o@example.com",
+      },
+      attendances: [
+        {
+          session: 1,
+          date: "2023-01-01",
+          status: "present",
+        },
+        {
+          session: 2,
+          date: "2023-01-02",
+          status: "absent",
+        },
+        {
+          session: 3,
+          date: "2023-01-03",
+          status: "sick",
+        },
+      ],
+    },
+    {
+      name: "English",
+      grade: "B",
+      teacher: {
+        name: "Ms. Johnson",
+        email: "rj4t2@example.com",
+      },
+      attendances: [
+        {
+          session: 1,
+          date: "2023-01-01",
+          status: "present",
+        },
+        {
+          session: 2,
+          date: "2023-01-02",
+          status: "absent",
+        },
+        {
+          session: 3,
+          date: "2023-01-03",
+          status: "on_leave",
+        },
+      ],
+    },
+  ],
+};
 
 export default function Home() {
+  const user = {
+    name: session?.user.name ?? "",
+    email: session?.user.email ?? "",
+    avatar: session?.user.image ?? "",
+  };
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+    // <div className="hidden lg:flex flex-col items-center justify-center min-h-screen max-w-screen bg-secondary">
+    //   <header className="flex w-full justify-between items-center px-10 py-4 border-b border-b-sidebar-border bg-sidebar">
+    //     <ul className="flex w-fit">
+    //       <li>Home</li>
+    //       <li>Published Works</li>
+    //       <li>Students</li>
+    //       <li>Home</li>
+    //     </ul>
+    //     {/* Auth actions */}
+    //     <div className="flex gap-2 w-fit items-center justify-end">
+    //       {isAuthenticated ? (
+    //         <>
+    //           {session?.user.role === Role.PARENT && (
+    //             <Button variant="outline" asChild>
+    //               <Link href="/admin">Console</Link>
+    //             </Button>
+    //           )}
+    //           <Avatar className="outline outline-offset-2">
+    //             <Image src="/user.svg" alt="user-icon" width={48} height={48} />
+    //           </Avatar>
+    //         </>
+    //       ) : (
+    //         <>
+    //           <Button variant="outline">Sign in</Button>
+    //           <Avatar className="rounded-full border" />
+    //         </>
+    //       )}
+    //     </div>
+    //   </header>
+
+    //   <div className="flex flex-1 justify-center h-full w-full">
+    //     <div className="flex flex-1 flex-col h-full items-start py-4 bg-sidebar-primary-foreground border-r border-r-sidebar-border">
+    //       <div className="flex px-8 items-start justify-between flex-1">
+    //         <Avatar className="outline w-36 h-36">
+    //           <AvatarImage
+    //             src="/user.svg"
+    //             alt="user-icon"
+    //             width={48}
+    //             height={48}
+    //           />
+    //         </Avatar>
+    //         <Edit className="text-primary" size={20} />
+    //       </div>
+    //       <div className="flex flex-col max-w-full justify-start gap-4 px-8">
+    //         <div className="space-x-2">
+    //           <h1 className="text-2xl font-semibold">{data.student.name}</h1>
+    //           <p className="text-sm text-gray-500">{data.student.email}</p>
+    //         </div>
+    //         <div className="space-x-2">
+    //           <p>{data.student.nisn}</p>
+    //           <p>
+    //             {new Date(data.student.dateOfBirth).toLocaleDateString("id-ID")}
+    //           </p>
+    //           <p>{data.student.phoneNumber}</p>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <div className="flex flex-col w-full">
+    //       <div className="flex flex-1 items-center justify-center">
+    //         <h1 className="text-xl font-semibold text-primary">
+    //           General Studies
+    //         </h1>
+    //       </div>
+    //       <div className="flex flex-1 items-center justify-center">remarks</div>
+    //     </div>
+    //   </div>
+    // </div>
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "20vw",
+        } as React.CSSProperties
+      }
+    >
+      <Sidebar
+        collapsible="icon"
+        className="overflow-hidden *:data-[sidebar=sidebar]:flex-row"
+      >
+        <Sidebar
+          collapsible="none"
+          className="w-[calc(var(--sidebar-width-icon)+1px)]! border-r"
+        >
+          <SidebarContent>
+            <SidebarGroup>
+              <SidebarGroupContent className="px-1.5 md:px-0">
+                <SidebarMenu>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton>
+                      <HomeIcon />
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </SidebarGroup>
+          </SidebarContent>
+        </Sidebar>
+        <Sidebar collapsible="none">
+          <SidebarContent>
+            <SidebarGroup className="flex flex-col gap-2">
+              <SidebarGroupContent className="px-1.5 md:px-0">
+                <SidebarMenu className="flex flex-col gap-2">
+                  <SidebarMenuItem className="flex justify-between items-start">
+                    <Avatar className="outline w-24 h-24">
+                      <AvatarImage src="/user.svg" alt="user-icon" />
+                    </Avatar>
+                    {session?.user.role !== Role.PARENT && (
+                      <Button variant="ghost" size="icon" asChild>
+                        <Link href="/admin/students">
+                          <Edit />
+                        </Link>
+                      </Button>
+                    )}
+                  </SidebarMenuItem>
+                  <SidebarMenuItem className="flex flex-col">
+                    <h1 className="text-lg font-semibold">
+                      {data.student.name}
+                    </h1>
+                    <p className="text-sm text-secondary-foreground">
+                      {data.student.email}
+                    </p>
+                  </SidebarMenuItem>
+                  <SidebarMenuItem className="flex flex-col gap-1 text-sm font-normal text-gray-500">
+                    <p>{data.student.nisn}</p>
+                    <p>
+                      {new Date(data.student.dateOfBirth).toLocaleDateString(
+                        "id-ID"
+                      )}
+                    </p>
+                    <p>{data.student.phoneNumber}</p>
+                  </SidebarMenuItem>
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </SidebarGroup>
+            <Separator />
+            <SidebarGroup>
+              <SidebarGroupContent className="px-1.5 md:px-0">
+                <SidebarMenu>
+                  <SidebarMenuItem></SidebarMenuItem>
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </SidebarGroup>
+          </SidebarContent>
+          <SidebarFooter>
+            <NavUser user={user} />
+          </SidebarFooter>
+        </Sidebar>
+      </Sidebar>
+      <SidebarInset>
+        <header className="bg-background sticky top-0 flex justify-between items-center border-b p-4">
+          <div className="flex shrink-0 gap-2 items-center">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">All Inboxes</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Inbox</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+          <div className="flex shrink-0 gap-2 items-center">
+            <InputGroup>
+              <InputGroupInput placeholder="Search..." />
+              <InputGroupAddon>
+                <SearchIcon />
+              </InputGroupAddon>
+            </InputGroup>
+            <Button variant="ghost" size="icon" asChild>
+              <Link href="/admin/students">
+                <MoonIcon />
+              </Link>
+            </Button>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4">
+          {/* {Array.from({ length: 24 }).map((_, index) => (
+            <div
+              key={index}
+              className="bg-muted/50 aspect-video h-12 w-full rounded-lg"
+            />
+          ))} */}
         </div>
-      </main>
-    </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
