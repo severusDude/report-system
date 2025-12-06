e370f13394d4de4df423b94dcf0d0dfe1c27b2be
severusDude <fajarsaladin05@gmail.com>
2025-12-06 22:17:44 +0700

    fix(auth): update redirect auth

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,4 +1,18 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await auth.api.getSession({ headers: await headers() });
+
+  if (session) {
+    redirect("/");
+  }
+
   return (
     <div className="grid w-screen min-h-screen overflow-hidden max-w-screen lg:grid-cols-2">
       <div className="flex items-center justify-center flex-1 w-full min-h-screen p-6">
--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,19 +1,5 @@
-"use client";
-
-import { redirect } from "next/navigation";
-import { authClient } from "@/lib/auth-client";
 import LoginForm from "@/components/forms/login-form";
 
-export default function Page() {
-  const { data: session, isPending } = authClient.useSession();
-
-  if (isPending) {
-    return <p>Loading...</p>;
-  }
-
-  if (session) {
-    redirect("/");
-  }
-
+export default async function Page() {
   return <LoginForm className="w-full max-w-xs" />;
 }
